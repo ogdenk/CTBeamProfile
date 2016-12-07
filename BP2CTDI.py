79107ccd63723093aac12c0c375d49e3b990e1ea
Kent Ogden <ogdenk@upstate.edu>
2016-12-07 10:13:03 -0500

    Added BP2CTDI program

--- BP2CTDI.py
+++ BP2CTDI.py
@@ -5,6 +5,8 @@ Program to calculate the CTDI in air as a function of radial distance from isoce
 import numpy
 import math
 from numpy import sin, cos, tan, pi, arctan2, sqrt, arccos
+import scipy
+from scipy.interpolate import CubicSpline
 
 BP = [1.0, 0.84211, 0.55526, 0.24868, 0.15, 0.10921, 0.08289 ] # Measured beam profile values (normalized)
 d = [0., 5., 10., 15., 20., 25., 30.]  # Measurement positions
@@ -15,12 +17,13 @@ r = numpy.sqrt(numpy.square(d)+57**2.0)  # radial distance from focal spot to me
 
 P = BP*numpy.square(r)/57.0**2.0 # Beam Profile corrected to constant 57 cm distance from FS
 
-p = numpy.arange(0,360) # 360 angular tube positions to calculate the CTDI
+a = numpy.arange(0,360) # 360 angular tube positions to calculate the CTDI
 
-Tx = 57*cos(p*numpy.pi/180) # Focal Spot x coordinates
-Ty = 57*sin(p*numpy.pi/180) # Focal Spot y coordinates
+Tx = -57*sin(a*numpy.pi/180) # Focal Spot x coordinates (0 degrees is overhead so use sin instead of cos)
+Ty = 57*cos(a*numpy.pi/180) # Focal Spot y coordinates
 
+CubicSpline.
 
-
-print (Tx, Ty)
+print (Tx)
+print (Ty)
 
