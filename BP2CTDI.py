2b831ad6e7f20513bdc63e155d46b869b737d7a7
Kent Ogden <ogdenk@upstate.edu>
2016-12-08 11:34:21 -0500

    Fixed bugs, changed to PCHIP cubic interpolation to allow extrapolation.  Interp1d can't extrapolate when using cubic spline.  Program seems to be working properly now.

--- BP2CTDI.py
+++ BP2CTDI.py
@@ -3,34 +3,55 @@ Program to calculate the CTDI in air as a function of radial distance from isoce
 """
 
 import numpy as np
-import math
 from numpy import sin, cos, tan, pi, arctan2, sqrt, arccos
-import scipy
 from scipy import interpolate
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
-BP = np.array([1.0, 0.84211, 0.55526, 0.24868, 0.15, 0.10921, 0.08289 ]) # Measured beam profile values (normalized)
+fs2iso = 57.0 # Focal spot to isocenter distance in cm
+BP = np.array([1.0, 0.84211, 0.55526, 0.24868, 0.15, 0.10921, 0.08289 ]) # Measured beam profile values on x axis (normalized)
 d = np.array([0., 5., 10., 15., 20., 25., 30.])  # Measurement positions
 
-theta = arctan2(d, 57.0) # Angular position of the beam profile measured values relative to central ray
+theta = arctan2(d, fs2iso) # Angular position of the beam profile values relative to central ray
 
-r = np.sqrt(np.square(d)+57**2.0)  # radial distance from focal spot to measurement point
+r = np.sqrt(np.square(d)+fs2iso**2.0)  # radial distance from focal spot to measurement point
 
-P = BP*np.square(r)/57.0**2.0 # Beam Profile corrected to constant 57 cm distance from FS
+P = BP*np.square(r)/fs2iso**2.0 # Beam Profile corrected to constant fs2iso cm distance from FS
 
-profile = interpolate.interp1d(theta, P,  kind = "cubic") # cubic spline fit of BP vs theta
+profile = interpolate.PchipInterpolator(theta, P, extrapolate = True) # cubic spline fit of BP vs theta
 
 ctdi = np.zeros(d.size)
 
-for x in  np.arange(0,d.size):
+for x in  np.arange(0,d.size): # for each measured BP value, calculate ctdi at that same location
     for a in np.arange(0,360): # 360 angular tube positions to calculate the CTDI
-        Tx = -57 * sin(a * np.pi / 180)  # Focal Spot x coordinates (0 degrees is overhead so use sin instead of cos)
-        Ty = 57 * cos(a * np.pi / 180)  # Focal Spot y coordinates
+        Tx = -fs2iso * sin(a * np.pi / 180)  # Focal Spot x coordinates (0 degrees is overhead so use sin instead of cos)
+        Ty = fs2iso * cos(a * np.pi / 180)  # Focal Spot y coordinates
         dp = np.dot([-Tx,-Ty],[d[x]-Tx,-Ty])
-        Phi = arccos(dp/(np.sqrt((d[x]-Tx)**2+Ty**2)*np.sqrt(Tx**2+Ty**2)))
-        ctdi[x] = ctdi[x] + profile(Phi)*(57**2)/np.sqrt((d[x]-Tx)**2+Ty**2)**2
+        r= np.sqrt((d[x]-Tx)**2+Ty**2)
+        cosTheta=dp/(fs2iso*r)
+        if (cosTheta >1):
+            cosTheta = 1
+        Phi = arccos(cosTheta)
+        ctdi[x] += profile(Phi)*(fs2iso**2)/np.sqrt((d[x]-Tx)**2+Ty**2)**2 # accumulate inverse square corrected contribution
 
-plt.plot(d,ctdi, 'o', d, BP, '-')
+maxval = ctdi.max()
+ctdi = ctdi/maxval
+
+dSmooth = np.linspace(0,d.max(), 100)
+BPSmooth = interpolate.spline(d, BP, dSmooth,kind = 'smoothest')
+ctdiSmooth = interpolate.spline(d, ctdi, dSmooth,kind = 'smoothest')
+
+
+red_patch = mpatches.Patch(color='red', label='Beam Profile')
+green_patch = mpatches.Patch(color='green', label='CTDI')
+plt.legend(handles=[green_patch, red_patch])
+
+
+plt.plot(d,ctdi, 'o', ms=10)
+plt.plot(d, BP, '+', ms=10)
+plt.plot(dSmooth, BPSmooth, 'r', label='Beam Profile')
+plt.plot(dSmooth, ctdiSmooth, 'g', label='CTDI')
+
 plt.show()
 
 print (d)
